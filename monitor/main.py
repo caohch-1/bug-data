59370c22fb35002284bda497606f801cb361b009
caohch-1 <2267408610@qq.com>
2023-08-30 19:19:02 +0800

    F1 trace log collected

--- monitor/main.py
+++ monitor/main.py
@@ -1,3 +1,17 @@
 from log_extractor import LogExtractor
+from trace_extractor import TraceExtractor
+from datetime import datetime, timedelta
+
+trace_extractor = TraceExtractor("http://localhost:9411/zipkin/")
+
+end_time = datetime.now()
+start_time = end_time - timedelta(minutes=120)
+limit = 100
+
+trace_extractor.get_traces(start_time, end_time, limit)
+trace_extractor.process_traces()
+trace_extractor.save_traces("./traces/F1/buggy")
+    
+
 log_extractor = LogExtractor()
-log_extractor.extract_all("./logs/F1/Buggy")
+log_extractor.extract_all("./logs/F1/buggy")
