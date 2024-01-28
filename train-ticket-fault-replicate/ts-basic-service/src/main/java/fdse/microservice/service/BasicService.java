9b7135251d18ec07951e6066ba1def226b375bf4
MissSirius <57584450+caohch-1@users.noreply.github.com>
2024-01-29 00:41:44 +0800

    HB4729

--- train-ticket-fault-replicate/ts-basic-service/src/main/java/fdse/microservice/service/BasicService.java
+++
@@ -1,9 +0,0 @@
-package fdse.microservice.service;
-
-import fdse.microservice.domain.*;
-
-
-public interface BasicService {
-    ResultForTravel queryForTravel(QueryForTravel info);
-    String queryForStationId(QueryStation info);
-}
